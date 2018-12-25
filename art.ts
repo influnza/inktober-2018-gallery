5e2b1bb540517f8b14f74a2025eb427ba8c8626e
influnza <olakisa@gmail.com>
2018-12-24 23:24:28 -0800

    Initial commit: basic WebVR view

---
+++ art.ts
@@ -0,0 +1,5 @@
+class Art {
+    constructor(imagePath: string, audioPath: string) {
+    }
+    
+}
---
+++ gallery.ts
@@ -0,0 +1,85 @@
+///<reference path="babylon.d.ts" />
+///<reference path="art.ts" />
+
+class Game {
+    private _canvas: HTMLCanvasElement;
+    private _engine: BABYLON.Engine;
+    private _scene: BABYLON.Scene;
+    private _camera: BABYLON.FreeCamera;
+    private _light: BABYLON.Light;
+
+    constructor(canvasElementId : string) {
+        // Create canvas and engine.
+        this._canvas = document.getElementById(canvasElementId) as HTMLCanvasElement;
+        this._engine = new BABYLON.Engine(this._canvas, true);
+    }
+  
+    createScene() : void {
+        // Create a basic BJS Scene object.
+        this._scene = new BABYLON.Scene(this._engine);
+
+        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
+        this._camera = new BABYLON.WebVRFreeCamera('camera1', new BABYLON.Vector3(0, 0, 0), this._scene);
+
+        // Target the camera to scene origin.
+        //this._camera.setTarget(BABYLON.Vector3.Zero()); not for vr camera
+
+        // Attach the camera to the canvas.
+        this._scene.onPointerDown = function () {
+            console.log("pointer down");
+            this._scene.onPointerDown = undefined;
+            this._camera.attachControl(this._canvas, true);
+        }.bind(this);
+
+        // Create a basic light, aiming 0,1,0 - meaning, to the sky.
+        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this._scene);
+
+        // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
+        let plane = BABYLON.MeshBuilder.CreatePlane('plane',
+                                    {width: 2, height: 3}, this._scene);
+
+        let meshAudio = new BABYLON.Sound("ambient", "resources/audio/Jam17-Ambient.wav", this._scene, null, {
+            loop: false, autoplay: true,
+            useCustomAttenuation: true,
+            maxDistance: 15
+        });
+        meshAudio.setAttenuationFunction(function (currentVolume, currentDistance, maxDistance, refDistance, rolloffFactor) {
+            if (currentDistance > maxDistance) return 0;
+            if (currentDistance == 0) {
+                return currentVolume;
+            }
+            return currentVolume * maxDistance / currentDistance;
+        });
+        meshAudio.attachToMesh(plane);
+        // Move the sphere upward 1/2 of its height.
+        plane.position.y = 1;
+        plane.position.z = 5;
+
+        // Create a built-in "ground" shape.
+        let ground = BABYLON.MeshBuilder.CreateGround('ground',
+                                {width: 6, height: 6, subdivisions: 2}, this._scene);
+    }
+  
+    doRender() : void {
+        // Run the render loop.
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
+        });
+
+        // The canvas/window resize event handler.
+        window.addEventListener('resize', () => {
+            this._engine.resize();
+        });
+    }
+  }
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    // Create the game using the 'renderCanvas'.
+    let game = new Game('renderCanvas');
+  
+    // Create the scene.
+    game.createScene();
+  
+    // Start render loop.
+    game.doRender();
+  });
